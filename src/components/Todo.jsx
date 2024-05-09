b502a1f1979b30926cf904af2a85540233d969b6
Umair Shaikh <imumairshaikh07@gmail.com>
2024-05-09 18:20:14 +0530

    done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import { TaskList } from "./TaskList";
 // import { DEL_ICON } from "../constants/images";
 import { DEL_ICON } from "../constants/images";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const getItemFromLocalStorage = () => {
+  let data = localStorage.getItem("tasks");
+
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
+
 function Todo() {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState(getItemFromLocalStorage());
 
   const [subject, setSubject] = useState("");
   const [hours, setHours] = useState("");
@@ -11,7 +23,30 @@ function Todo() {
   const handleTask = () => {
     if (!subject || !hours) {
       // if subject or hours is empty
+      toast.warn("Fill all the fields", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       return;
+    }
+
+    if (subject !== "" && hours !== "") {
+      toast.success("Task added successfully", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
 
     const reqObj = {
@@ -43,7 +78,22 @@ function Todo() {
   const handleDelete = (key) => {
     let updatedTask = task.filter((task, index) => index !== key);
     setTask(updatedTask);
+    toast.success("Task deleted successfully", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   };
+
+  useEffect(() => {
+    let addData = localStorage.setItem("tasks", JSON.stringify(task));
+    console.log(addData);
+  }, [task]);
 
   return (
     <div className="flex flex-col justify-center items-center mt-24">
@@ -52,7 +102,7 @@ function Todo() {
       </h1>
       <div className="flex gap-4 mt-10">
         <input
-          className="p-2 border-black border-2 rounded"
+          className="p-3 border-black border-2 rounded"
           type="text"
           placeholder="Subject"
           value={subject}
@@ -75,9 +125,12 @@ function Todo() {
 
       <div>
         {task.map((tasks, index) => (
-          <div className="flex justify-between w-[530px] mt-5" key={index}>
+          <div
+            className="flex justify-between w-[530px] mt-7 bg-[#29B475] text-white p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] hover:border-2  hover:border-[#29B475] hover:cursor-pointer transition-all ease-in-out"
+            key={index}
+          >
             <div className="flex gap-2 text-xl">
-              <span className="font-bold">{tasks.subject}</span>
+              <span className="font-bold ml-3">{tasks.subject}</span>
               <span> - </span>
               <span>{tasks.hours} Hours</span>
             </div>
@@ -97,7 +150,7 @@ function Todo() {
                 -
               </button>
               <button
-                className="text-3xl w-8 hover:bg-red-500 hover:rounded-lg"
+                className="text-3xl w-8 hover:bg-red-500 hover:rounded-lg text-[#fff]"
                 onClick={() => handleDelete(index)}
               >
                 {DEL_ICON}
@@ -106,6 +159,8 @@ function Todo() {
           </div>
         ))}
       </div>
+
+      <ToastContainer />
     </div>
   );
 }
